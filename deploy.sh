5e97747bd1ab5cbf6b33e157bce1478ed23c1967
TheTame <96096907+fanwang551@users.noreply.github.com>
2023-09-24 18:35:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,17 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'www.bookbook.cc' > CNAME
+echo 'chatgpt7292.eu.org' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f  https://github.com/penggang-home/penggang-home.gitub.io.git master:gh-pages
+# git push -f  https://github.com/fanwang551/fanwang/vuepress-blog.gitub.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # 请把 penggang-home/blog-two.git 换成你的项目地址
-# git push -f git@github.com:penggang-home/blog-two.git master:gh-pages
+# git push -f git@github.com:fanwang551/vuepress-blog.git master:gh-pages
 
 cd -
